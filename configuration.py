9d93676554fe081bf8a513be4ea3714a0e75d6fc
Ee ola <jpiletskaja@gmail.com>
2023-09-21 17:17:46 +0500

    fu

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://4998cfea-8973-4628-a7a3-cb9766468b55.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
